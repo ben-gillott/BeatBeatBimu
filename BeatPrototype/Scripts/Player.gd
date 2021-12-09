91451087b7cf837745b18a15f85faf8c1e19ec7b
Benjamin Gillott <bg357@cornell.edu>
2021-12-09 00:56:23 -0800

    State Machine Revamp, Tweak Movement (FIGHTER BASICS DONE)

--- BeatPrototype/Scripts/Player.gd
+++ BeatPrototype/Scripts/Player.gd
@@ -1,57 +1,72 @@
 extends KinematicBody2D
 
-export (int) var speed = 500
+export (int) var speed = 350
 export (int) var jump_speed = -500
 export (int) var gravity = 500
-export (float, 0, 1.0) var friction = 0.2
-export (float, 0, 1.0) var acceleration = 0.50
+export (float, 0, 1.0) var friction = 0.3
+export (float, 0, 1.0) var acceleration = 0.30
 
+enum {
+	MOVE,
+	ATTACK,
+	JUMP
+}
+var state = MOVE
 var velocity = Vector2.ZERO
-var dir = 0;
+var dir = 0
+
 onready var originalScaleX = scale.x
-onready var stateMachine = $AnimationTree.get("parameters/playback")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
-var isAttacking = false
 
-func get_input():
-	dir = 0
-	if Input.is_action_pressed("player_right"):
-		dir = 1
-		$Sprite.scale.x = dir
-	if Input.is_action_pressed("player_left"):
-		dir = -1
-		$Sprite.scale.x = dir
+func _ready():
+	animationTree.active = true
 
-	
-
-		
-	if dir != 0:
-		velocity.x = lerp(velocity.x, dir * speed, acceleration)
-	else:
-		velocity.x = lerp(velocity.x, 0, friction)
-	
-	
-	#if(dir == 1): #face left
-		#$Sprite.flip_h = true
-	#if(dir == -1): #face right
-		#$Sprite.flip_h = false
-	
-	if Input.is_action_just_pressed("player_attack"):
-		stateMachine.travel("attack_2")
-	elif(dir != 0): #Running
-		stateMachine.travel("run")
-	else: #Stopped
-		stateMachine.travel("idle")
-		
-	
 
 func _physics_process(delta):
-	get_input()
+	match(state):
+		MOVE:
+			move_state(delta)
+		JUMP:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+
+func move_state(delta):
+	var inputX = 0
+	inputX = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
+	
+	if inputX != 0: #Moving
+		#Flip Sprite
+		if inputX < 0:
+			$Sprite.scale.x = -1
+		else:
+			$Sprite.scale.x = 1
+		
+		animationState.travel("run")
+		
+		velocity.x = lerp(velocity.x, inputX * speed, acceleration)
+	else:
+		animationState.travel("idle")
+		velocity.x = lerp(velocity.x, 0, friction)
+		
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("player_jump"):
-		if is_on_floor():
-			velocity.y = jump_speed
+	
+	if Input.is_action_just_pressed("player_attack"):
+		state = ATTACK
 
+func attack_state(delta):	
+	velocity = Vector2.ZERO
+	animationState.travel("attack_2")
+
+
+func attack_animation_finished():
+	state = MOVE
+	
 func onHurt(area):
 	print("player hurt")
+	
