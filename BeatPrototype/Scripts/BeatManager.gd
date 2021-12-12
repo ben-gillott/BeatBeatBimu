ed5fa9e6ed17f3408fdf1e0410fc661d8b1fe8ce
Benjamin Gillott <bg357@cornell.edu>
2021-12-11 23:04:12 -0800

    Beat Calclations + Beat Visualizer

--- BeatPrototype/Scripts/BeatManager.gd
+++ BeatPrototype/Scripts/BeatManager.gd
@@ -1,18 +1,23 @@
 extends Node2D
 
 
-var bpm = 100.0
+var bpm = 100.0 #Tombtorial is 100bpm
 var spb = 60.0/bpm
 
-var beat = 0
+var beatRangeTolerance = .1 #In beat to one side
 var beatLoopLength = 4
 
+var isBeating = false
 var time = 0.0 #Dont reset this, make sure to keep in time with song.
+var beat = 0
 
-# Called when the node enters the scene tree for the first time.
+#Local use
+var beatLoopCount
+var beatOffset #how far away from nearest beat, in beats
+var nearestBeat
+
 func _ready():
-	spb = 60/bpm
-	pass # Replace with function body.
+	startBeat()
 
 func startBeat():
 	time = 0
@@ -21,5 +26,26 @@ func startBeat():
 func _process(delta):
 	time += delta
 	beat = time/spb
+	
+	nearestBeat = int(round(beat))
+	beatLoopCount = nearestBeat % beatLoopLength
+	beatOffset = abs(nearestBeat - beat)
+	
+	#If beat float is within range - is beating
+	if beatOffset < beatRangeTolerance:
+		isBeating = true
+	else:
+		isBeating = false
+	
+	#printCurrent()
+	#if changes - trigger signal?
+	
+	
+func printCurrent():
 	print("time: " + str(time))
 	print("beat: " + str(beat))
+	print("nearest: " + str(nearestBeat))
+	print("beat count: " + str(beatLoopCount))
+	print("offset: " + str(beatOffset))
+	print("\n")
+	
---
+++ BeatPrototype/Scripts/BeatVisualizer.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+onready var centerScaleStart = $Center.scale
+var centerScaleUpRatio = 1.5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	if get_parent().isBeating:
+		centerIsBeating()
+	else:
+		centerIsNotBeating()
+	#manually check parent isBeating and call sub funcs accordingly
+	pass
+
+func centerIsBeating():
+	$Center.modulate = Color(.5,1,.5)
+	$Center.scale = centerScaleStart * centerScaleUpRatio
+
+func centerIsNotBeating():
+	$Center.modulate = Color(1,1,1)
+	$Center.scale = centerScaleStart
+	
+
