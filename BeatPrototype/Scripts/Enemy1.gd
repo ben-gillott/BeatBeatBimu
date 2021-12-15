af81decf4f3d40777df0dfd2a0ba39d35aafd5fe
Benjamin Gillott <bg357@cornell.edu>
2021-12-14 17:04:05 -0800

    Hop start

--- BeatPrototype/Scripts/Enemy1.gd
+++ BeatPrototype/Scripts/Enemy1.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
+
+var targetPosition = null
+var speed = 5
+var hopDistance = -100
+
+func _ready():
+	hopForward()
+	
 func onHurt(area):
 	print("enemy hurt")
 	queue_free()
+
+func hopForward():
+	targetPosition = global_position.x + hopDistance
+
+func _process(delta):
+	if targetPosition != null:
+		global_position.x = lerp(global_position.x, targetPosition, delta * speed)
